a52142d6ce18595db141e7796ab3f418eb981054
sivasanjaycse <sivasanjaidisco@gmail.com>
2026-02-28 14:56:21 +0530

    Changed cors policy

--- backEnd/src/server.js
+++ backEnd/src/server.js
@@ -10,7 +10,27 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      const allowedOrigins = [
+        "http://localhost:5173",
+        "http://localhost:3000",
+        "http://20.204.117.91",
+      ];
+
+      // allow requests with no origin (like Postman)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  }),
+);
 app.use(express.json({ limit: "10mb" })); // Increased limit to handle base64 images
 
 // Routes
